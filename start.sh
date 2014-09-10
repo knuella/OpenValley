41779bf1b1b8511894dbd222f37a6e8d368c6815
knuella <robin.meier@gmx.net>
2014-09-11 00:21:32 +0200

    a lot of merging and cleaning

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
 	cd base/src
 	python3 message_broker.py&
 	echo $! > $path/mb.pid
-	python3 configuration_manager_mongodb_alt.py cm tcp://127.0.0.10:6666 tcp://127.0.0.10:6665 tcp://127.0.0.10:5556 tcp://127.0.0.10:5555&
+	python3 configuration_manager_mongodb.py cm tcp://127.0.0.10:6666 tcp://127.0.0.10:6665 tcp://127.0.0.10:5556 tcp://127.0.0.10:5555&
 	echo $! > $path/cm.pid
 	python3 template_manager.py tm tcp://127.0.0.10:6666 tcp://127.0.0.10:6665 tcp://127.0.0.10:5556 tcp://127.0.0.10:5555&
 	echo $! > $path/tm.pid
