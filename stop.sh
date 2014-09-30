e5a8f16ccc23bf87c857fe295632f6cde2468eb5
Akigiri <Akigiri@users.noreply.github.com>
2014-09-30 21:26:20 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-pidfiles="mb.pid cm.pid tm.pid visu.pid digital_output.pid analog_output.pid digital_input.pid"
+pidfiles="mb.pid cm.pid tm.pid visu.pid digital_output.pid analog_output.pid digital_input.pid 0x20_1.pid 0x20_2.pid 0x20_3.pid 0x20_4.pid gpio_7.pid gpio_11.pid gpio_12.pid gpio_13.pid"
 
 for pidfile in $pidfiles
 do
